b606a9d1300a205993fd2bb6140ecfc86eb89c01
Nguyễn Hoàng Long <53606646+Mew-Kyu@users.noreply.github.com>
2023-04-05 22:10:15 +0700

    Update Register.jsx

--- src/components/layout/login/Register.jsx
+++ src/components/layout/login/Register.jsx
@@ -139,6 +139,20 @@ export const Register = () => {
                   required: true,
                   message: "Please input your password!",
                 },
+                {
+                min: 6,
+                message: "Password must be at least 8 characters long",
+                },
+                {
+                  max: 15,
+                  message: "Password cannot be more than 30 characters long",
+                },
+                {
+                  pattern:
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                  message:
+                    "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character",
+                },
               ]}
               hasFeedback
             >
